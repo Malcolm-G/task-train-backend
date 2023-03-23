3371b619808d0afdd11c612ee3e10c91f0551d02
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-23 11:20:40 +0300

    created login and logout features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,36 @@
 class UsersController < ApplicationController
+    before_action :session_expired?, only: [:check_login_status]
 
     def register
         user = User.create(user_params)
         if user.valid?
+            save_user(id:user.id)
             app_response(message:'Registration was successful', status: :created, data:user)
         else
             app_response(message: 'Something went wrong during registration', status: :unprocessable_entity, data:user.errors)
         end
+    end
+
+    def login
+        # user = User.find_by(username:user_params[:username])
+        sql = 'username = :username OR email = :email'
+        user = User.where(sql,{username:user_params[:username], email:user_params[:email]}).first
+
+        if user&.authenticate(user_params[:password])
+            save_user(id:user.id)
+            app_response(message:'Login was successful', status: :ok, data:user)
+        else
+            app_response(message:'Invalid username/email or password', status: :unauthorized)
+        end
+    end
+
+    def logout
+        remove_user
+        app_response(message:'Logout was successful', status: :ok)
+    end
+
+    def check_login_status
+        app_response(message:'success', status: :ok)
     end
 
     private
